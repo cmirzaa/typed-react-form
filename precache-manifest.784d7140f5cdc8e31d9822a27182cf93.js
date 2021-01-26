554dd003b3aa828ccc7184823d58721940985ea5
Stijn Rogiest <reddusted@gmail.com>
2021-01-26 17:50:50 +0100

    Updates

---
+++ precache-manifest.784d7140f5cdc8e31d9822a27182cf93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706283916831faeafa02608cfd77e845",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a411b969af30d894c9ec",
+    "url": "./static/css/main.e624ec94.chunk.css"
+  },
+  {
+    "revision": "ddc9ba6a5cc2ca820003",
+    "url": "./static/js/2.48ca4a58.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.48ca4a58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a411b969af30d894c9ec",
+    "url": "./static/js/main.f84a95cf.chunk.js"
+  },
+  {
+    "revision": "5582da3441c32c2ed488",
+    "url": "./static/js/runtime-main.6e93dfba.js"
+  }
+]);
